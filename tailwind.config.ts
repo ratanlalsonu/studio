1ae0d941c79e16555a5cf7e510651eda9575765c
Ratan Lal Gaund <lal96560@gmail.com>
2025-11-07 09:28:57 +0000

    unchange the text apnadairy logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
       fontFamily: {
         body: ['PT Sans', 'sans-serif'],
         headline: ['PT Sans', 'sans-serif'],
-        logo: ['Lobster', 'cursive'],
+        logo: ['PT Sans', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
