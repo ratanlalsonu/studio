da36ca53ba54af2a36805db9223340c70a6cb852
Ratan Lal Gaund <lal96560@gmail.com>
2025-11-07 13:18:51 +0000

    http://5.imimg.com/data5/SELLER/Default/2022/11/WD/NC/VW/15933885/fresh-

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**',
       },
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
     ],
   },
 };
